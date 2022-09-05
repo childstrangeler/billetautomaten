ee78ebd59f5efb08adb0f69ea3b32bf192748bdc
Jesper <jesperbogoemoe@gmail.com>
2022-09-05 16:12:00 +0200

    oprettet scanner

--- src/App.java
+++ src/App.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class App {
 
     // billetautomaten tager konsol argumenter som adminpris
     public static void main(String[] args) throws Exception {
-        System.out.println("Køb billet plis UwU");
+        System.out.println("Hvilken billet vil du købe?");
+
+        Scanner userinput = new Scanner(System.in);
+
+        String input = userinput.nextLine();
+
+        System.out.println("du skrev: " + input);
+
     }
 }
