8afa730743364c59324d84f007e5cc4bba0015dd
Jesper <jesperbogoemoe@gmail.com>
2022-09-05 15:31:37 +0200

    initial commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+
+    // billetautomaten tager konsol argumenter som adminpris
+    public static void main(String[] args) throws Exception {
+        System.out.println("Køb billet plis UwU");
+    }
+}
