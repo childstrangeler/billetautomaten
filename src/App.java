6ac163c85247c512e11b4072d566a7ddf675a4ac
Jesper <jesperbogoemoe@gmail.com>
2022-09-07 13:28:30 +0200

    bare ye

--- src/App.java
+++ src/App.java
@@ -1,10 +1,27 @@
 import java.util.Scanner;
 
+/*
+K1) Når en automat opstilles på en station skal installatøren kunne angive prisen per
+billet. Efter installation skal prisen være fast.
+K2) Automaten skal kunne fortælle kunden hvad en billet koster.
+K3) Automaten skal kunne udskrive en billet mærket med DSB’s nye logo (i
+terminalen).
+K4) Automaten vil til enhver tid kunne vise kunden balancen, dvs. hvor mange penge
+kunden har puttet i automaten.
+K5) Ved opstilling skal installatøren kunne angive balancen, sådan at installatøren
+Programmering i Java. 3. HTX – Programmering B – 2022/23. Lærer: Steen Grøntved
+kan udskrive en eller flere test-billetter uden at skulle putte penge i automaten.
+K6) Medarbejderen skal kunne aflæse hvor mange billetter der er blevet solgt af
+automaten og kunne tømme den for penge.
+ */
+
 public class App {
 
     public String fraby = "";
     public String tilby = "";
     static public int zonepris = 14;
+    public String adminuser = "admin";
+    public String adminkey = "admin";
 
     // billetautomaten tager konsol argumenter som adminpris
     public static void main(String[] args) throws Exception {
