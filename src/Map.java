1038fb9cc31d68e94877c6c4eca8599e0796e657
unic0rn9k <unic0rn9k@protonmail.com>
2022-09-12 14:56:07 +0200

    Fixed Map constructor

--- src/Map.java
+++ src/Map.java
@@ -8,12 +8,12 @@ public class Map {
 
   public Zone[] map;
   public Map() {
-    map = {
-        new Zone({tp("C", 1), tp("m1", 2)}),
-        new Zone({tp("C", 2), tp("m1", 0)}),
-        new Zone({tp("C", 0), tp("m3", 0)}),
+    map = new Zone[] {
+        new Zone(new TransportLinje[] {tp("C", 1), tp("m1", 2)}),
+        new Zone(new TransportLinje[] {tp("C", 2), tp("m1", 0)}),
+        new Zone(new TransportLinje[] {tp("C", 0), tp("m3", 0)}),
     };
   }
 
-  public Zone[] quickest_route(int from, int to) { new Zone[](0) }
+  public Zone[] quickest_route(int from, int to) { return new Zone[0]; }
 }
